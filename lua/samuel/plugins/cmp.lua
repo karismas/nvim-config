a8dc2c3be8713de1cb2f574c0ddd5e894deeb1cb
karismas <hubers001@gmail.com>
2024-09-13 18:17:55 -0400

    Working on some Python LSP stuff

--- lua/samuel/plugins/cmp.lua
+++ lua/samuel/plugins/cmp.lua
@@ -46,6 +46,14 @@ return {
 		config = function()
 			local cmp = require('cmp')
 			require("luasnip.loaders.from_vscode").lazy_load()
+			-- THE FOLLOWING ALMOST WORKS BUT TAKES WAY TOO MUCH WORK
+			-- cmp.event:on('menu_opened',
+			-- 	function()
+			-- 		if BLA then
+			-- 			local keys = vim.api.nvim_replace_termcodes("<C-k><CR>", true, true, true)
+			-- 			vim.api.nvim_feedkeys(keys, "m", false)
+			-- 		end
+			-- 	end)
 			cmp.setup({
 				experimental = {
 					ghost_text = true
@@ -60,23 +68,81 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				sources = {
-					{name = 'nvim_lsp'},
 					{name = 'luasnip'},
+					{name = 'nvim_lsp'},
 					-- {name = 'buffer'},
 					-- {name = 'path'},
 				},
 				--- (Optional) Show source name in completion menu
-				formatting = cmp_format,
+				-- formatting = cmp_format,
 				mapping = cmp.mapping.preset.insert({
 					['<C-i>'] = cmp.mapping.select_prev_item(),
 					['<C-k>'] = cmp.mapping.select_next_item(),
+					['<C-d>'] = function()
+						if cmp.visible_docs() then
+							cmp.close_docs()
+						else
+							cmp.open_docs()
+						end
+					end,
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-q>'] = cmp.mapping.abort(),
+					-- ['<C-f>'] = function()
+					-- 	cmp.abort()
+					-- 	cmp.complete()
+					-- end,
 					['<CR>'] = cmp.mapping.confirm({ select = false }),
-				})
 
+					-- THE FOLLOWING ALMOST WORKS BUT TAKES WAY TOO MUCH WORK
+					-- ['<CR>'] = function()
+
+					-- 	if cmp.get_active_entry()['resolved_completion_item'] == nil then
+					-- 		if BLA then
+					-- 			local choice = cmp.get_entries()[1]['completion_item']['sortText']
+					-- 			if choice == nil then
+					-- 				local keys = vim.api.nvim_replace_termcodes("<C-q>", true, true, true)
+					-- 				vim.api.nvim_feedkeys(keys, "m", false)
+					-- 			else
+					-- 				if string.find(choice, CHOICE) then
+					-- 					cmp.confirm({ select = true })
+					-- 				end
+					-- 			end
+					-- 		end
+					-- 		BLA = false
+					-- 		return
+					-- 	end
+
+					-- 	CHOICE = cmp.get_active_entry()['resolved_completion_item']['insertText']
+					-- 	if CHOICE == nil then
+					-- 		cmp.confirm({ select = false })
+					-- 		return
+					-- 	end
+
+					-- 	local found = string.find(CHOICE, "%$0")
+					-- 	if found then
+					-- 		cmp.confirm({ select = false })
+					-- 		return
+					-- 	else
+					-- 		cmp.abort()
+					-- 		cmp.complete()
+					-- 		BLA = true
+					-- 	end
+					-- end
+				}),
+
+				-- sorting = {
+				-- 	comparators = {
+				-- 		-- cmp.config.compare.kind,
+				-- 		cmp.config.compare.exact,
+				-- 		-- cmp.config.compare.offset,
+				-- 		-- cmp.config.compare.score,
+				-- 		cmp.config.compare.sort_text,
+				-- 		-- cmp.config.compare.length,
+				-- 		-- cmp.config.compare.order,
+				-- 	}
+				-- }
 			})
 		end
 	}
--- lua/samuel/plugins/nvim-treesitter.lua
+++ lua/samuel/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
 			auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
-			indent = { enable = true },  
+			indent = { enable = true },
 			incremental_selection = {
 				enable = true,
 				keymaps = {
